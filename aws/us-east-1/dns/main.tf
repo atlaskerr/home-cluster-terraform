cff41cb8e809739a7fb9af148e76c97b411458f1
Atlas Kerr <atlaskerr@gmail.com>
2018-10-27 13:53:30 -0500

    dns: added enron.io private domain

---
+++ aws/us-east-1/dns/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-enron"
+    key    = "aws/us-east-1/dns/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-enron"
+    key    = "aws/us-east-1/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+locals {
+  vpc_id          = "${data.terraform_remote_state.vpc.vpc_id}"
+  private_zone_id = "${aws_route53_zone.internal.id}"
+}
+
+resource "aws_route53_zone" "internal" {
+  name    = "enron.io"
+  comment = "Enron Internal DNS"
+  vpc_id  = "${local.vpc_id}"
+}
+
+output "private_zone_id" {
+  value = "${local.private_zone_id}"
+}
